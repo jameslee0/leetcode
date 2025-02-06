73093648af4e53bc192ae786d8b45dbe33b08d03
James Lee <james.98.lee@gmail.com>
2025-02-06 00:02:46 -0500

    wip for leetcode daily

--- Quan/testing.js
+++ Quan/testing.js
@@ -32,3 +32,27 @@ function removeDuplicates(nums) {
 const nums = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5];
 console.log(removeDuplicates(nums));
 
+// Hi
+
+function areAlmostEqual(s1, s2) {
+    let offset1 = '';
+    let counter = 0;
+    let swap = false;
+
+    return s1 === s2;
+
+    for (i = 0; i < s1.length; i++) {
+        if (s1[i] !== s2[i]) {
+            counter++;
+            if (counter > 2) return false;
+            if (offset1 == s2[i]) {
+                swap = true;
+            }
+            offset1 = s1[i];
+        }
+    }
+
+    if (swap) return true;
+
+    return false;
+}
