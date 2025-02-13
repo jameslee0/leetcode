217b65be22889097a4395ea9ff4275ab31bbf96f
unknown <james.98.lee@gmail.com>
2025-02-13 01:37:55 -0500

    dailies

--- Quan/testing.js
+++ Quan/testing.js
@@ -235,4 +235,21 @@ function readBinaryWatch(turnedOn) {
     return results;
 };
 
-hi
+//Leetcode Daily
+
+function maximumSum(nums) { 
+    let max = new Array(82).fill(0);
+    let ans = -1;
+    for (let x of nums) {
+        let sum = 0, temp = x;
+        while (temp !== 0) {
+            sum += temp % 10;
+            temp = Math.floor(temp / 10);
+        }
+        if (max[sum] !== 0) ans = Math.max(ans, x + max[sum]);
+        max[sum] = Math.max(max[sum], x);
+    }
+    return ans;
+};
+maximumSum([3, 6, 8, 5]);
+
