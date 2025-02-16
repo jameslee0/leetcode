28c96ea35ff5595461fc9fd8ca22d20d7ab7be3b
jameslee0 <james.98.lee@gmail.com>
2025-02-16 03:43:45 -0500

    added dijkstras algo code

--- Quan/testing.js
+++ Quan/testing.js
@@ -351,39 +351,39 @@ maximumSum([3, 6, 8, 5]);
 // };
 // minOperations([1, 2, 3, 4, 5, 6], 7);
 
-var ProductOfNumbers = function() {
-    this.stack = [1];
-};
+// var ProductOfNumbers = function() {
+//     this.stack = [1];
+// };
 
-/** 
- * @param {number} num
- * @return {void}
- */
-ProductOfNumbers.prototype.add = function(num) {
-    if(num === 0) {
-        this.stack = [1];
-    }
-    else {
-        const index = this.stack.length;
+// /** 
+//  * @param {number} num
+//  * @return {void}
+//  */
+// ProductOfNumbers.prototype.add = function(num) {
+//     if(num === 0) {
+//         this.stack = [1];
+//     }
+//     else {
+//         const index = this.stack.length;
 
-        const prevNum = this.stack[index - 1];
+//         const prevNum = this.stack[index - 1];
 
-        this.stack.push(prevNum * num);
-    }
-};
+//         this.stack.push(prevNum * num);
+//     }
+// };
 
-/** 
- * @param {number} k
- * @return {number}
- */
-ProductOfNumbers.prototype.getProduct = function(k) {
-    const length = this.stack.length;
+// /** 
+//  * @param {number} k
+//  * @return {number}
+//  */
+// ProductOfNumbers.prototype.getProduct = function(k) {
+//     const length = this.stack.length;
 
-    if(k >= length) {
-        return 0;
-    }
-    return this.stack[length - 1] / this.stack[length - 1 - k]
-};
+//     if(k >= length) {
+//         return 0;
+//     }
+//     return this.stack[length - 1] / this.stack[length - 1 - k]
+// };
 
 /** 
  * Your ProductOfNumbers object will be instantiated and called as such:
@@ -392,65 +392,181 @@ ProductOfNumbers.prototype.getProduct = function(k) {
  * var param_2 = obj.getProduct(k)
  */
 
-function canPartition(s, target) {
-    if (s === "" && target === 0) return true;
-    if (target < 0) return false;
-    let ans = false;
-    for (let i = 0; i < s.length; i++) {
-      let left = s.substring(0, i + 1);
-      let right = s.substring(i + 1);
-      let leftNum = parseInt(left);
+// function canPartition(s, target) {
+//     if (s === "" && target === 0) return true;
+//     if (target < 0) return false;
+//     let ans = false;
+//     for (let i = 0; i < s.length; i++) {
+//       let left = s.substring(0, i + 1);
+//       let right = s.substring(i + 1);
+//       let leftNum = parseInt(left);
   
-      let isPossible = canPartition(right, target - leftNum);
-      if (isPossible) {
-        ans = true;
-        break;
-      }
-    }
-    return ans;
-  }
-  function punishmentNumber(n) {
-    let sum = 0;
-    for (let num = 1; num <= n; num++) {
-      let sqr = num * num;
-      if (canPartition(sqr.toString(), num)) {
-        sum += sqr;
-      }
-    }
-    return sum;
-  }
+//       let isPossible = canPartition(right, target - leftNum);
+//       if (isPossible) {
+//         ans = true;
+//         break;
+//       }
+//     }
+//     return ans;
+//   }
+//   function punishmentNumber(n) {
+//     let sum = 0;
+//     for (let num = 1; num <= n; num++) {
+//       let sqr = num * num;
+//       if (canPartition(sqr.toString(), num)) {
+//         sum += sqr;
+//       }
+//     }
+//     return sum;
+//   }
 
-  var constructDistancedSequence = function(n) {
-    const arr = new Array(2 * n - 1).fill(null); 
-    const used = new Set(); 
+//   var constructDistancedSequence = function(n) {
+//     const arr = new Array(2 * n - 1).fill(null); 
+//     const used = new Set(); 
     
-    function backtrack(index) {
-        if (index === arr.length) return [...arr]; 
-        if (arr[index] !== null) return backtrack(index + 1); 
+//     function backtrack(index) {
+//         if (index === arr.length) return [...arr]; 
+//         if (arr[index] !== null) return backtrack(index + 1); 
 
-        for (let num = n; num >= 1; num--) { 
-            if (used.has(num)) continue; 
-            if (num === 1) { 
-                arr[index] = 1;
-                used.add(1);
-                const result = backtrack(index + 1);
-                if (result) return result; 
-                arr[index] = null; 
-                used.delete(1);
-            } else { 
-                if (index + num < arr.length && arr[index + num] === null) {
-                    arr[index] = arr[index + num] = num; 
+//         for (let num = n; num >= 1; num--) { 
+//             if (used.has(num)) continue; 
+//             if (num === 1) { 
+//                 arr[index] = 1;
+//                 used.add(1);
+//                 const result = backtrack(index + 1);
+//                 if (result) return result; 
+//                 arr[index] = null; 
+//                 used.delete(1);
+//             } else { 
+//                 if (index + num < arr.length && arr[index + num] === null) {
+//                     arr[index] = arr[index + num] = num; 
 
-                    used.add(num);
-                    const result = backtrack(index + 1);
-                    if (result) return result; 
-                    arr[index] = arr[index + num] = null; 
-                    used.delete(num);
-                }
+//                     used.add(num);
+//                     const result = backtrack(index + 1);
+//                     if (result) return result; 
+//                     arr[index] = arr[index + num] = null; 
+//                     used.delete(num);
+//                 }
+//             }
+//         }
+//         return null;
+//     }
+
+//     return backtrack(0);
+// };
+
+// IMPORTANTTT!!!!!!!!
+// DIJKSTRA'S ALGORITHM
+let V = 9;
+
+// A utility function to find the 
+// vertex with minimum distance 
+// value, from the set of vertices 
+// not yet included in shortest 
+// path tree 
+function minDistance(dist,sptSet)
+{
+    
+    // Initialize min value 
+    let min = Number.MAX_VALUE;
+    let min_index = -1;
+    
+    for(let v = 0; v < V; v++)
+    {
+        if (sptSet[v] == false && dist[v] <= min) 
+        {
+            min = dist[v];
+            min_index = v;
+            console.log(["if statement hit", "dist", dist[v], "min", min, "min_index", min_index])
+        }
+        else{
+            console.log(["v Counter", v, "dist", dist[v], "min", min, "min_index", min_index])
+        }
+    }
+    console.log("min")
+    console.log(min_index)
+    return min_index;
+}
+
+// A utility function to print 
+// the constructed distance array 
+function printSolution(dist)
+{
+    console.log("Vertex \t\t Distance from Source<br>");
+    for(let i = 0; i < V; i++)
+    {
+        console.log(i + " \t\t " + 
+                 dist[i] + "<br>");
+    }
+}
+
+// Function that implements Dijkstra's 
+// single source shortest path algorithm 
+// for a graph represented using adjacency 
+// matrix representation 
+function dijkstra(graph, src)
+{
+    let dist = new Array(V);
+    let sptSet = new Array(V);
+    
+    // Initialize all distances as 
+    // INFINITE and stpSet[] as false 
+    for(let i = 0; i < V; i++)
+    {
+        dist[i] = Number.MAX_VALUE;
+        sptSet[i] = false;
+    }
+    
+    // Distance of source vertex 
+    // from itself is always 0 
+    dist[src] = 0;
+    
+    // Find shortest path for all vertices 
+    for(let count = 0; count < V - 1; count++)
+    {
+        
+        // Pick the minimum distance vertex 
+        // from the set of vertices not yet 
+        // processed. u is always equal to 
+        // src in first iteration. 
+        let u = minDistance(dist, sptSet);
+        
+        // Mark the picked vertex as processed 
+        sptSet[u] = true;
+        
+        // Update dist value of the adjacent 
+        // vertices of the picked vertex. 
+        console.log(["dist", dist])
+        for(let v = 0; v < V; v++)
+        {
+            
+            // Update dist[v] only if is not in 
+            // sptSet, there is an edge from u 
+            // to v, and total weight of path 
+            // from src to v through u is smaller 
+            // than current value of dist[v] 
+            if (!sptSet[v] && graph[u][v] != 0 && 
+                   dist[u] != Number.MAX_VALUE &&
+                   dist[u] + graph[u][v] < dist[v])
+            {
+                dist[v] = dist[u] + graph[u][v];
             }
         }
-        return null;
+        console.log(["post for loop", dist])
     }
+    
+    // Print the constructed distance array
+    printSolution(dist);
+}
 
-    return backtrack(0);
-};
+// Driver code
+let graph = [ [ 0, 4, 0, 0, 0, 0, 0, 8, 0 ],
+              [ 4, 0, 8, 0, 0, 0, 0, 11, 0 ],
+              [ 0, 8, 0, 7, 0, 4, 0, 0, 2 ],
+              [ 0, 0, 7, 0, 9, 14, 0, 0, 0],
+              [ 0, 0, 0, 9, 0, 10, 0, 0, 0 ],
+              [ 0, 0, 4, 14, 10, 0, 2, 0, 0],
+              [ 0, 0, 0, 0, 0, 2, 0, 1, 6 ],
+              [ 8, 11, 0, 0, 0, 0, 1, 0, 7 ],
+              [ 0, 0, 2, 0, 0, 0, 6, 7, 0 ] ]
+dijkstra(graph, 0);
