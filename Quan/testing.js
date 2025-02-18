23190b546b54a937a5c5a0152aec9f3ee80d3a61
jameslee0 <james.98.lee@gmail.com>
2025-02-17 23:14:46 -0500

    dailies

--- Quan/testing.js
+++ Quan/testing.js
@@ -572,3 +572,17 @@ let graph = [ [ 0, 4, 0, 0, 0, 0, 0, 8, 0 ],
 dijkstra(graph, 0);
 
 //Changes for testing
+
+var smallestNumber = function(pattern) {
+    let ans = ["1"], temp = [];
+    for (let i = 0; i < pattern.length; i++) {
+        if (pattern[i] === 'I') {
+            ans.push(...temp.reverse(), (i + 2).toString());
+            temp = [];
+        } else {
+            temp.push(ans.pop());
+            ans.push((i + 2).toString());
+        }
+    }
+    return ans.concat(temp.reverse()).join("");
+};
