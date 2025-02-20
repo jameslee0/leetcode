e211b21018cd91d7afbbe5e48d13bd77cbf96789
jameslee0 <james.98.lee@gmail.com>
2025-02-19 23:12:00 -0500

    dailies

--- Quan/testing.js
+++ Quan/testing.js
@@ -603,3 +603,11 @@ var getHappyString = function(n, k) {
 
     return dfs("", n, k);
 };
+
+var findDifferentBinaryString = function(nums) {
+    let result = "";
+    for (let i = 0; i < nums.length; i++) {
+        result += nums[i][i] === '0' ? '1' : '0';
+    }
+    return result;    
+};
