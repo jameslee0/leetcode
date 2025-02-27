e257107181a98bbec3c43c19ddd56611fd303c1c
jameslee0 <james.98.lee@gmail.com>
2025-02-26 23:01:49 -0500

    daily

--- Quan/testing.js
+++ Quan/testing.js
@@ -812,3 +812,17 @@ var maxAbsoluteSum = function(nums) {
     }
     return res;
 };
+
+const lenLongestFibSubseq = (arr) => {
+    const set = new Set(arr);
+    let maxLength = 0;
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            for (let a = arr[i], b = arr[j], len = 3; set.has(a + b); len++) {
+                [a, b] = [b, a + b];
+                maxLength = Math.max(maxLength, len);
+            }
+        }
+    }
+    return maxLength;
+};
