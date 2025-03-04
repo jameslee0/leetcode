ef93d3177b4befd67473e6aea31de04b16e745c1
jameslee0 <james.98.lee@gmail.com>
2025-03-03 20:28:58 -0500

    dailies

--- Quan/testing.js
+++ Quan/testing.js
@@ -826,3 +826,12 @@ const lenLongestFibSubseq = (arr) => {
     }
     return maxLength;
 };
+var checkPowersOfThree = function(n) {
+    while (n > 0) {
+        if (n % 3 == 2) {
+            return false;
+        }
+        n = Math.round(n / 3);
+    }
+    return true;
+}
